47b76826d56b6ebf5ef1397ab8ec763b89c9c224
OloladeKajopinola <kololadeitunnu@gmail.com>
2023-09-13 11:53:11 +0000

    restatement of function

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure each tuple has at least 2 elements and use 0 for missing integers
+    result = ()
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
     
