68200118f8837eac48b206bd19e4a0496246db01
OloladeKajopinola <kololadeitunnu@gmail.com>
2023-09-15 15:32:38 +0000

    still trying to pass checker

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import dis
 
 if __name__ == "__main__":
-    module_code = __import__("hidden_4").__doc__
-    names = [name for name in dir(module_code) if not name.startswith('__')]
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-    for name in sorted(names):
-        print(name)
-
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
