4217259f85dfdd884f87cca49fdbdbe3abff52c1
OloladeKajopinola <kololadeitunnu@gmail.com>
2023-09-15 15:23:15 +0000

    4th task

--- 4-new_in_list.py
+++ 4-new_in_list.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        my_list.reverse()
-        for w in range(len(my_list)):
-            print("{:d}".format(my_list[w]))
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
