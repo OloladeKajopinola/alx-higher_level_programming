e9708cec30d2c738722c991ed1392581345173aa
OloladeKajopinola <kololadeitunnu@gmail.com>
2023-09-16 10:54:30 +0000

    passing checker

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    # Check if the sentence is empty
-    if not sentence:
-        return (0, None)  # Return a tuple with length 0 and None for the first character
-    
-    # Get the length of the sentence and the first character
-    length = len(sentence)
-    first_char = sentence[0]
-    
-    return (length, first_char)
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
